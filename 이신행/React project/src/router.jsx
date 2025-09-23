1d3fb9778adba2d16d1da0fb2ac90c1823e5eae5
shinhaeng1204@gmail.com <shinhaeng1204@gmail.com>
2025-09-23 10:05:35 +0900

    이신행 테스트

---
+++ 이신행/React project/src/router.jsx
@@ -0,0 +1,34 @@
+import { lazy } from "react";
+import { Routes, Route } from "react-router";
+
+import RootLayout from "./layouts/RootLayout";
+
+import { ROUTES } from "@/lib/routes";
+import AuthLayout from "@/routes/auth/AuthLayout";
+import Login from "@/routes/auth/Login";
+import Register from "@/routes/auth/Register";
+import Home from "@/routes/Home";
+import NotFound from "@/routes/NotFound";
+import SwithCash from "@/routes/project/SwithCash";
+
+const Profile = lazy(() => import("@/routes/member/Profile"));
+export default function AppRoutes() {
+  return (
+    <Routes>
+      <Route element={<RootLayout />}>
+        <Route index element={<Home />} />
+        <Route path={ROUTES.MEMBER.ROOT}>
+          <Route path={ROUTES.MEMBER.PROFILE} element={<Profile />} />
+        </Route>
+        <Route element={<AuthLayout />}>
+          <Route path={ROUTES.AUTH.LOGIN} element={<Login />} />
+          <Route path={ROUTES.AUTH.REGISTER} element={<Register />} />
+        </Route>
+        <Route path={ROUTES.PROJECT.ROOT}>
+          <Route path={ROUTES.PROJECT.SWITHCASH} element={<SwithCash />} />
+        </Route>
+        <Route path={ROUTES.NOT_FOUND} element={<NotFound />} />
+      </Route>
+    </Routes>
+  );
+}
